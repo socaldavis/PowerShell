5dd3a1b3ce940b0172306c77043934cc2a486ad4
socaldavis <socaldavis@gmail.com>
2013-05-11 06:48:17 -0500

    Initial commit with test folders.

---
+++ folder-1/advanced-functions.ps1
@@ -0,0 +1,36 @@
+﻿function Get-Stuff {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position=0,Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
+        [Alias('host')]
+        $computerName
+    )
+    BEGIN {}
+    PROCESS {
+        if ($PSBoundParameters.ContainsKey('computerName')) {
+            # we got parameter input
+            foreach ($name in $computerName) {
+                StuffWorker $name
+            }
+        } else {
+            # we got pipline input
+            StuffWorker $_
+        }     
+    }
+    END {}
+}
+
+function StuffWorker {
+    param (
+        $computerName
+    )
+    Get-WmiObject -Class win32_BIOS -computerName $computerName
+}
+
+# Usage:
+# Get-Stuff server-r2
+# Get-Stuff -computername server-r2
+# Get-Stuff -computername (Get-Content names.txt)
+# Get-Content names.txt | Get-Stuff
+# Get-ADComputer -filter * |
+#      Select @{l='ComputerName';e={$_.Name}} | Get-Stuff
